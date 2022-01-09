7c7921b907a2b8c3df5a7308296841ca2c23185f
saurabh <saurabhj8087445@gmail.com>
2022-01-09 15:46:00 +0530

    add polyfill code without setTimeout

--- polyfill_promises/index.js
+++ polyfill_promises/index.js
@@ -1,24 +1,32 @@
 class CustomPromise {
   constructor(executor) {
     this.value = null;
-    this.onFulfilled = null;
+      this.onFulfilled = null;
+      this.isFulfilled = false;
+      this.isCalled = false;
 
     const resolve = (value) => {
-      this.value = value;
-      this.onFulfilled(this.value);
+        this.value = value;
+        this.isFulfilled = true;
+
+        if (typeof this.onFulfilled === 'function') {
+            this.onFulfilled(this.value);
+            this.isCalled = true;
+        }
     };
 
     executor(resolve);
   }
 
   then(cb) {
-    this.onFulfilled = cb;
+      this.onFulfilled = cb;
+      if (this.isFulfilled) {
+          this.onFulfilled(this.value);
+      }
     return this;
   }
 }
 
 const result = new CustomPromise((resolve) => {
-  setTimeout(function () {
-    resolve("Hello");
-  }, 2000);
+    resolve("Hello");   
 }).then((data) => console.log(data));
